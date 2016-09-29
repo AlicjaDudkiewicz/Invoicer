05f780895edf8ce108a8e26f335556ecf4f51c1d
Alicja Dudkiewicz <alicja.dudkiewicz.123@gmail.com>
2016-09-29 12:33:45 +0200

    Fixed method of identify password and dependencies in pom.xml.

--- src/pl/adudkiewicz/services/UserService.java
+++ src/pl/adudkiewicz/services/UserService.java
@@ -1,5 +1,6 @@
 package pl.adudkiewicz.services;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +17,20 @@ public class UserService
     {
 
         User user = userRepository.findOne(inputUser.getUsername());
-        if (user != null)
+        if (user != null && verifyPassword(inputUser, user))
         {
             verifyPassword(inputUser, user);
             return user;
         }
         return null;
     }
-
+ 
     private boolean verifyPassword(User inputUser, User user)
     {
         String correctPassword = user.getPassword();
         String givenPassword = inputUser.getPassword();
-        if (correctPassword.equals(givenPassword))
+        String passwordDigest = DigestUtils.sha256Hex(givenPassword);
+        if (correctPassword.equals(passwordDigest))
         {
             return true;
         }
